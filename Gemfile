7d68f47c94e255173479c490fe8409110de58406
jessyon <kateyon@gmail.com>
2016-04-03 17:28:09 +1000

    Added will_paginate gem and setup pagination

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'devise', '~> 3.5', '>= 3.5.6'
 gem 'paperclip', '~> 4.3', '>= 4.3.6'
 #gem 'aws-sdk', '~> 2.2', '>= 2.2.28'
 gem 'aws-sdk', '< 2.0'
+gem 'will_paginate', '~> 3.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -21,7 +21,9 @@ before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
 
 	def show
-		@episodes =  Episode.where(podcast_id: @podcast).order("created_at DESC").reject {|e| e.id == @episode.id}
+		@episodes =  Episode.where(podcast_id: @podcast).order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+		# .reject {|e| e.id == @episode.id}
+
 	end
 
 	def edit
